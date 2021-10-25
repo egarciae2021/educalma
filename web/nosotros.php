458d572d50e534aeda5668226c29236e397e2d21
Jean Matos Palomino <jeanzz.1910@gmail.com>
2021-10-25 23:13:11 +0000

    Update web/nosotros.php

--- web/nosotros.php
+++ web/nosotros.php
@@ -1,11 +1,11 @@
 <?php include_once 'includes/Inicio/Head.php' ?>
 
 <body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
-<?php include_once 'includes/Inicio/Loader.php' ?>
+<?php //include_once 'includes/Inicio/Loader.php' ?>
 
 <?php include_once 'includes/Inicio/Header.php' ?>
 
-<?php //include_once 'includes/nosotros/Noscontenido.php' ?>
+<?php include_once 'includes/nosotros/Noscontenido.php' ?>
 
 <?php include_once 'includes/Inicio/Footer.php' ?>
 
