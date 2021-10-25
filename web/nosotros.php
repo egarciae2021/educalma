7f33477ee44a2afbccd8261b6c198d8a1df4691d
Angell4S <asencios093@gmail.com>
2021-10-25 22:28:16 +0000

    Update web/nosotros.php

--- web/nosotros.php
+++ web/nosotros.php
@@ -1,7 +1,7 @@
 <?php include_once 'includes/inicio/Head.php' ?>
 
 <body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
-<?php include_once 'includes/inicio/Loader.php' ?>
+<?php include_once 'includes/inicio/loader.php' ?>
 
 <?php include_once 'includes/inicio/Header.php' ?>
 
