99aff8bb614b156f3832f8fcd66de2519e98f98c
Angell4S <asencios093@gmail.com>
2021-10-25 22:11:17 +0000

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
-<?php include_once 'includes/Inicio/head.php' ?>
+<?php include_once 'includes/Inicio/Head.php' ?>
 
 <body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
 <?php //include_once 'includes/inicio/loader.php' ?>
-<?php include_once 'includes/Inicio/header.php' ?>
+<?php include_once 'includes/Inicio/Header.php' ?>
 
 <?php include_once 'includes/Inicio/Contenido.php' ?>
 <?php include_once 'includes/Inicio/Footer.php' ?>
