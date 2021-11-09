7c303b73539894098ca6c82a482efc0c27fc517f
Jean Matos Palomino <jeanzz.1910@gmail.com>
2021-11-09 04:58:30 +0000

    Update web/video.php, web/recursos1.php, web/progreso.php, web/nosotros.php, web/miscursos.php, web/iniciosesion.php, web/index.php, web/foro.php, web/editartema.php, web/editarcurso.php, web/editarModulo.php, web/detallecurso.php, web/descargas.php, web/cursoinscrito.php, web/curso.php, web/cuestionario.php, web/anuevocurso.php, web/agregartema.php, web/agregarcurso.php, web/agregarModulos.php, web/agregarCategorias.php, web/ListaCursos.php, web/Form_respue_cuestionario.php, web/Form_pregun_cuestionario.php, web/Editar_respue_cuestionario.php, web/Editar_pregun_Cuestionario.php, web/Cursoiniciar.php

--- web/editarcurso.php
+++ web/editarcurso.php
@@ -1,10 +1,10 @@
-<?php include_once 'includes/inicio/head.php' ?>
+<?php include_once 'includes/Inicio/head.php' ?>
 
 <body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
-<?php include_once 'includes/inicio/loader.php' ?>
-<?php include_once 'includes/inicio/header.php' ?>
+<?php include_once 'includes/Inicio/loader.php' ?>
+<?php include_once 'includes/Inicio/header.php' ?>
 
 <?php include_once 'includes/curso/CursoEditar-View.php' ?>
-<?php include_once 'includes/inicio/Footer.php' ?>
+<?php include_once 'includes/Inicio/Footer.php' ?>
 
 </body>
