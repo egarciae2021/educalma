8e255261c0e8adcb4ffc5a13a5f403d95cb98c82
Jean Matos Palomino <jeanzz.1910@gmail.com>
2021-11-09 05:05:11 +0000

    Update web/video.php, web/registros.php, web/recursos1.php, web/progreso.php, web/nosotros.php, web/miscursos.php, web/iniciosesion.php, web/index.php, web/foro.php, web/editartema.php, web/editarcurso.php, web/editarModulo.php, web/detallecurso.php, web/descargas.php, web/cursoinscrito.php, web/curso.php, web/cuestionario.php, web/anuevocurso.php, web/agregartema.php, web/agregarcurso.php, web/agregarModulos.php, web/agregarCategorias.php, web/ListaCursos.php, web/Form_respue_cuestionario.php, web/Form_pregun_cuestionario.php, web/Editar_respue_cuestionario.php, web/Editar_pregun_Cuestionario.php, web/Cursoiniciar.php

--- web/editartema.php
+++ web/editartema.php
@@ -1,9 +1,9 @@
-<?php include_once 'includes/Inicio/head.php' ?>
+<?php include_once 'includes/Inicio/Head.php' ?>
 
 <body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
-<?php include_once 'includes/Inicio/loader.php' ?>
+<?php include_once 'includes/Inicio/Loader.php' ?>
 
-<?php include_once 'includes/Inicio/header.php' ?>
+<?php include_once 'includes/Inicio/Header.php' ?>
 
 <?php include_once 'includes/tema/editartemas.php' ?>
 
