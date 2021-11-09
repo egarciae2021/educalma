752ad1b6643c6d9eee73c90f90a378c23475556a
Jean Matos Palomino <jeanzz.1910@gmail.com>
2021-11-09 06:07:20 +0000

    Update web/iniciosesion.php

--- web/iniciosesion.php
+++ web/iniciosesion.php
@@ -1,15 +1,15 @@
 <?php include_once 'includes/Inicio/Head.php' ?>
 
-<body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
+
 <?php include_once 'includes/Inicio/Loader.php' ?>
 
-<?php 
-// Sesión
+<<?php 
+// --- Sesión
     ob_start();
     @session_start();
     $now = time();
     if(isset($_SESSION['acabo'])){
-      echo '<script>swal({title: "Su Sesión Terminó",icon: "warning",allowEscapeKey: false,allowOutsideClick:false,text: "Ingrese sus Datos Nuevamente para continuar.",showConfirmButton: false});</script>';
+      echo '<script>sesion_expirada();</script>';
       session_destroy(); 
     }
 ?>
@@ -23,7 +23,7 @@
     if(isset($_SESSION['Logueado'])){
         if($_SESSION['Logueado']===true){
             if(isset($_SESSION['expire']) && $now < $_SESSION['expire']){
-                echo '<script>window.location="index.php";</script>';   
+                echo '<script>window.location="user-sidebar.php";</script>';   
             }
         }else{
             if(isset($_SESSION['estado_actividad']) && $_SESSION['estado_actividad'] != 1){
@@ -32,7 +32,7 @@
               echo '</script>';
               session_destroy();
             } else{
-                echo '<script>swal("Falló Inicio de Sesión", "El Nombre de Usuario y/o Contraseña son Incorrectos.", "error");</script>';    
+                echo '<script>datos_incorrectos();</script>';    
                 session_destroy();
             }
         }
@@ -41,4 +41,4 @@
 ?>
 
 <?php include_once 'includes/Inicio/Footer.php' ?>
-</body>
+
