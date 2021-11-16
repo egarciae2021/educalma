86d98abf18bdfa761ee94b21fa62186c88007cc7
Jean Matos Palomino <jeanzz.1910@gmail.com>
2021-11-16 06:50:55 +0000

    Update web/includes/dashboard/body1.php

--- web/includes/dashboard/body1.php
+++ web/includes/dashboard/body1.php
@@ -1,7 +1,7 @@
 <?php
 // Este codigo hace validacion para que no se pueda acceder a cualquier pagina sin estar logueado__Pablo Loyola
 ob_start();
-    @session_start();
+    session_start();
      echo "hola mundo 2";
  if(isset($_SESSION['Logueado']) && ($_SESSION['Logueado'] === true)){
      echo "hola mundo";
