69a908d0a84e2ba73d6657e326779db516d956f0
Jean Matos Palomino <jeanzz.1910@gmail.com>
2021-11-16 05:51:52 +0000

    Update web/includes/login/checklogin.php

--- web/includes/login/checklogin.php
+++ web/includes/login/checklogin.php
@@ -56,12 +56,12 @@ if ($dato['estado'] == 1 && password_verify($password_sinHash, $pass_con_hash) =
     if($_SESSION['privilegio'] == 1 || $_SESSION['privilegio'] == 2 || $_SESSION['privilegio'] == 3 || $_SESSION['privilegio'] == 4 || $_SESSION['privilegio'] == 5 || $_SESSION['privilegio'] == 6){
         //echo '<script>swal("Inicio de Sesión Exitoso", "Has iniciado sesión correctamente.", "success");</script>';
          $_SESSION['Logueado']=true;
-        header('Location: ../../index.php'); 
+        //header('Location: ../../index.php'); 
        
     }else{
         $_SESSION['Logueado']=false;
         //echo '<script>swal("Falló el Inicio de Sesión", "El Nombre de Usuario y/o Contraseña son Incorrectos.", "error");</script>';
-        header('Location: ../../iniciosesion.php'); 
+        //header('Location: ../../iniciosesion.php'); 
         
     } 
 }else{
@@ -70,7 +70,8 @@ if ($dato['estado'] == 1 && password_verify($password_sinHash, $pass_con_hash) =
 
 if($_SESSION['Logueado'] != true){
     $_SESSION['usuario'] = $username;
-    header('Location: ../../iniciosesion.php');
+    //header('Location: ../../iniciosesion.php');
 }
+echo $_SESSION['nombres'];
 ob_end_flush();
 ?>
