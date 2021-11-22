834ff4ea32bc72e7ac3f9aa81a16a3a82bd83be5
Jean Matos Palomino <jeanzz.1910@gmail.com>
2021-11-22 22:59:24 +0000

    Update web/video.php, web/sidebarUserAdmin.php, web/sidebarEditar.php, web/sidebarCursos.php, web/registros.php, web/recursos1.php, web/progreso.php, web/nosotros.php, web/miscursos.php, web/foro.php, web/editartema.php, web/editarcurso.php, web/editarModulo.php, web/detallecurso.php, web/descargas.php, web/cursoinscrito.php, web/curso.php, web/cuestionario.php, web/certi.php, web/anuevocurso.php, web/agregartema.php, web/agregarcurso.php, web/agregarModulos.php, web/agregarCategorias.php, web/ListaCursos.php, web/Form_respue_cuestionario.php, web/Form_pregun_cuestionario.php, web/Editar_respue_cuestionario.php, web/Editar_pregun_Cuestionario.php, web/Cursoiniciar.php

--- web/certi.php
+++ web/certi.php
@@ -1,4 +1,7 @@
-
+<?php   
+    ob_start();
+    @session_start();
+?>
 <body>
 <?php include_once 'includes/reco-certi/recocerti.php' ?>
 </body>
--- web/miscursos.php
+++ web/miscursos.php
@@ -1,3 +1,7 @@
+<?php   
+    ob_start();
+    @session_start();
+?>
 <?php include_once 'includes/inicio/Head.php' ?>
 
 <body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
