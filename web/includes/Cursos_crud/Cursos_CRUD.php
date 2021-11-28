f30e3ec858d42bb5c7b8c9d993cb671b5ad50116
Mariella Loli <melolinunez@gmail.com>
2021-11-27 20:15:48 -0500

    Arreglo del guardado de datos al actualizar la fila imagen de Publicar Cursos _ Educalma, Mariella L.

--- web/includes/Cursos_crud/Cursos_CRUD.php
+++ web/includes/Cursos_crud/Cursos_CRUD.php
@@ -115,9 +115,16 @@
         $nomb=$_POST['nomb_actu_cursos'];
         $descr=$_POST['desc_curso'];
         $prec=$_POST['prec_curso'];
-        $nombreimagenes=$_FILES['txtimagenAct']['tmp_name'];
-        $nombreimagen=addslashes(file_get_contents($nombreimagenes));
-        
+	
+	    if(($_FILES['txtimagenAct']['size'])>0){
+            $nombreimagenes=$_FILES['txtimagenAct']['tmp_name'];
+        	$nombreimagen=addslashes(file_get_contents($nombreimagenes));
+            $pdo = Database::connect();
+            $consulta= $pdo->prepare("UPDATE cursos SET imagenDestacadaCurso='$nombreimagen' WHERE idCurso='$id'");
+            $consulta->execute();
+            Database::disconnect(); 
+        }
+
         if($_POST['prec_curso']<=0){
             $prec = 'Gratis';
         }else{
@@ -125,17 +132,18 @@
         }
         
         $pdo2 = Database::connect();
-        $consult1=$pdo2->prepare("UPDATE cursos SET nombreCurso='$nomb', descripcionCurso='$descr', costoCurso='$prec', imagenDestacadaCurso='$nombreimagen' WHERE idCurso = '$id' ");
+        $consult1=$pdo2->prepare("UPDATE cursos SET nombreCurso='$nomb', descripcionCurso='$descr', costoCurso='$prec' WHERE idCurso = '$id' ");
         $consult1->execute();
         Database::disconnect();
 
         echo'
             <script>
-                alert ("Actualizado exitosamente");
+                // alert ("Actualizado exitosamente");
                 window.location = "../../publicarcursos.php";
             </script>
         ';
-
+    
+        
     }
     
 
