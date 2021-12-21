9d3f3f13048d17a5ecf6c1d01a0af6c4ed2e7f5b
sheyla2112 <sheylinrey21@gmail.com>
2021-12-20 19:38:35 -0500

    pagepay pagina de pago, Reymundez S

---
+++ web/pagepay.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Pagina Pago</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/pagepay.css" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="contenedor-nextpage">
+            <img class="image-cont" src="assets/images/online-payment.png" alt="">
+            <h1 class="mt-4">¡Paso Final!</h1>
+            <hr>
+            <h3>Estas a punto de pagar con paypal la cantidad de:</h3>
+            <div class="my-3 mx-auto" style="max-width: 350px;">
+                <span>s/300.00</span><br>
+                <a href="" class="btn my-1 visa"><img src="assets/images/visa.png" alt=""></a><br>
+                <a href="" class="btn my-1 paypal"><img src="assets/images/paypal (1).png" alt=""></a><br>
+                <a href="" class="btn my-1 credit"><i class="fas fa-credit-card mr-2"></i>Debit or Credit Card</a><br>
+                <h5>La forma rápida y segura de pagar</h5>
+            </div>
+            <h4>Los productos podrán ser descargados una vez que se procede el pago</h4>
+            <h5>(Para aclaraciones :uputoamoestuvoaqui@gmail.com)</h5>
+        </div>
+    </div>
+</body>
+
+</html>
