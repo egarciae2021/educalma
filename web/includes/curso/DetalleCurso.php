84ba070d238d740cad3dd4311cc3ef4519ab90e3
sheyla2112 <sheylinrey21@gmail.com>
2021-12-21 02:25:24 -0500

    Pagina de detalle curso, Reymundez S

--- web/includes/curso/DetalleCurso.php
+++ web/includes/curso/DetalleCurso.php
@@ -1,244 +1,143 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detalle Cursos</title>
-    <link rel="stylesheet" href="././assets/css/styledetcurso.css">
+    <title>Bootstrap Example</title>
+
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styledetcurso.css">
 </head>
+
 <body>
 
-                <!--
-                    ======================================
-                               Detalle de Curso
-                    ======================================
-                -->
-<?php
-// Este codigo hace validacion para que no se pueda acceder a cualquier pagina sin estar logueado__Pablo Loyola
-
- //if(isset($_SESSION['Logueado']) && ($_SESSION['Logueado'] === true)){
-?>
-<?php
- 
+    <?php
     require_once 'database/databaseConection.php';
-    $id=$_GET['id'];
-
-    $pdo4 = Database::connect(); 
+    $id = $_GET['id'];
+    $pdo4 = Database::connect();
     $sql4 = "SELECT * FROM cursos WHERE idCurso='$id'";
     $q4 = $pdo4->prepare($sql4);
     $q4->execute(array());
     $dato4 = $q4->fetch(PDO::FETCH_ASSOC);
     Database::disconnect();
-?>
-
-<section class="contbody">
-
-    <div class="contdetinfo">
-        <div class="continfo">
-
-            <div class="detboxinfo">
-           <!-- Cursos prevención de bullying-->
-                <h1><?php echo $dato4['nombreCurso'];?> </h1>
-                <!-- <p><?php echo $dato4['descripcionCurso'];?></p> -->
-                
-                
-            </div>
-
-            <!-- <div class="detboxinfo2">
-                
-                <?php 
-                    $ProfesorID = $dato4['id_userprofesor'];
-
-                    //DATOS DEL PROFESOR
-                    $pdo5 = Database::connect(); 
-                    $sql5 = "SELECT * FROM usuarios WHERE id_user='$ProfesorID'";
-                    $q5 = $pdo5->prepare($sql5);
-                    $q5->execute(array());
-                    $dato5 = $q5->fetch(PDO::FETCH_ASSOC);
-
-                    $nombreP = $dato5['nombres'] . " " . $dato5['apellido_pat'] . " " . $dato5['apellido_mat'];
-                    $correoP = $dato5['email'];
-               ?>
-               <form class="formulario" action="" method="POST" enctype="multipart/form-data">
-               <div class="imgbx"><img src="data:image/*;base64,<?php echo base64_encode($dato5['mifoto']);?>" alt="foto_curso" style="width: 60px;"></div>
-                </form>
-                <h1><?php echo $nombreP ?></h1>
-                <p><?php echo $correoP ?></p>
-            </div> -->
-
-            <div class="detboxinfo3">
-                <h1>Porttitor feugiat</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Accumsan
-                    arcu ac adipiscing tincidunt adipiscing praesent consectetur. Sed
-                    amet, sapien metus, proin justo. Lacus adipiscing nisi, velit arcu.
-                    Consequat pretium, est tortor, proin urna nibh urna urna. Convallis
-                    integer ipsum vitae
-                </p>
-                <h1>Porttitor feugiat</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Accumsan
-                    arcu ac adipiscing tincidunt adipiscing praesent consectetur. Sed
-                    amet, sapien metus, proin justo. Lacus adipiscing nisi, velit arcu.
-                    Consequat pretium, est tortor, proin urna nibh urna urna. Convallis
-                    integer ipsum vitae
-                </p>
-                <h1>Porttitor feugiat</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Accumsan
-                    arcu ac adipiscing tincidunt adipiscing praesent consectetur. Sed
-                    amet, sapien metus, proin justo. Lacus adipiscing nisi, velit arcu.
-                    Consequat pretium, est tortor, proin urna nibh urna urna. Convallis
-                    integer ipsum vitae
-                </p>
-            </div>
-
-            <div class="detboxinfo4">
-                <h1>Acerca del curso</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Accumsan
-                    arcu ac adipiscing tincidunt adipiscing praesent consectetur. Sed
-                    amet, sapien metus, proin justo. Lacus adipiscing nisi, velit arcu.
-                    Consequat pretium, est tortor, proin urna nibh urna urna. Convallis
-                    integer ipsum vitae
-                </p>
-      
-            </div>
-
+    ?>
+    <div class="bg-dark1">
+        <div class="row py-4">
+            <br>
         </div>
     </div>
-
-    <div class="contboxdet">
-        <div class="contboxinfo">
-            <div class="detbox">
-                <img src="data:image/*;base64,<?php echo base64_encode($dato4['imagenDestacadaCurso'])?>">
-                     <a href="./includes/Cursos_crud/inscribeteCurso.php?id=<?php echo $id;?>">Inscríbete ahora</a>
-    
-                <h1>Detalles del curso</h1>
-                <?php
-                    $pdo10 = Database::connect(); 
-                    $sql10 = "SELECT * FROM cursoinscrito WHERE curso_id='$id'";
-                    $q10 = $pdo10->prepare($sql10);
-                    $q10->execute();
-                    $dato10 = $q10->rowCount();
-                    Database::disconnect();
-
-                    $pdo11 = Database::connect(); 
-                    $sql11 = "SELECT * FROM modulo WHERE id_curso='$id'";
-                    $q11 = $pdo11->prepare($sql11);
-                    $q11->execute();
-                    $dato11 = $q11->rowCount();
-                    Database::disconnect();
-
-                    if($dato10!=1){
-                        echo "<p>Este curso cuenta con <strong>".$dato10."</strong> usuarios inscritos<p>";
-                    }else{
-                        echo "<p>Este curso cuenta con <strong>".$dato10."</strong> usuario inscrito<p>";
-                    }
-
-                    if ($dato11!=1){
-                        echo "<p>Contiene <strong>".$dato11."</strong> Módulos<p>";
-                    }else{
-                        echo "<p>Contiene <strong>".$dato11."</strong> Módulo<p>";
-                    }
-                    
-                    echo '<p>Contiene un Certificado</p>';
-                   
-                ?>
-                
-               
+    <div class="bg-dark1">
+        <div class="row py-5">
+            <div class="col-12 col-sm-12 col-md-7 col-lg-8 col-xl-8 ">
+                <span>Desarrollo</span><i class="fas fa-angle-right mx-2"></i>
+                <span>Desarrollo web</span><i class="fas fa-angle-right mx-2"></i>
+                <span>Desarrollo web</span>
+                <h2 class="my-2 font-weight-bold">Universidad Desarrollo Web 2021 - FrontEnd Web Developer !</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque recusandae quod culpa maxime commodi
+                    dolor eaque fugit delectus provident optio deleniti quae, soluta voluptates repellat saepe nostrum
+                    dolore doloribus omnis.</p>
+                <i class="fas fa-stopwatch mr-2"></i><span>Fecha: Lorem ipsum dolor sit amet.</span>
+                <i class="fas fa-globe ml-4 mr-2"></i><span>Español</span>
+                <i class="fas fa-closed-captioning ml-4 mr-2"></i><span>Español [automático]</span>
             </div>
-        </div>
-    </div>
-
-</section>
-
-
-<section>
- <!-- start FAQ -->
-    <div class="containerdet">
-
-
-        <div class="row">
-
-                <div class="col-md-10">
-                    <div class="conttitudet">
-                    <h1 >Temario del curso</h1>
+            <div class="col-12 col-sm-12 col-md-5 col-lg-4 col-xl-4 info-course-right">
+                <div class="card" style="position: absolute;width: 90%; ">
+                    <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAPars5s2FQzySbkUpPjtBlvPlANAFDLP7x38q8nOqcke_Lrf_if34Y-kTjGQgS6pRvuQ&usqp=CAU" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                        <h4 class="card-title font-weight-bold" style="font-size: 30px;">s/ 49,90</h4>
+                        <button type="button" class="btn btn-outline-dark my-3">Comprar ahora</button>
+                        <p class="font-weight-bold mb-0">Este curso incluye:</p>
+                        <div class="my-1" style="font-size: 13px;">
+                            <div><i class="fab fa-youtube text-center" style="width: 1.5rem;"></i><span class="ml-3">Lorem
+                                    ipsum dolor sit.</span></div>
+                            <div><i class="far fa-file text-center" style="width: 1.5rem;"></i><span class="ml-3">Lorem
+                                    ipsum
+                                    dolor sit.</span></div>
+                            <div><i class="far fa-folder-open text-center" style="width: 1.5rem;"></i><span class="ml-3">Lorem
+                                    ipsum dolor sit.</span></div>
+                            <div><i class="fas fa-infinity text-center" style="width: 1.5rem;"></i><span class="ml-3">Lorem
+                                    ipsum dolor sit.</span></div>
+                            <div><i class="fas fa-mobile-alt text-center" style="width: 1.5rem;"></i><span class="ml-3">Lorem
+                                    ipsum dolor sit.</span></div>
+                            <div><i class="fas fa-clipboard-check text-center" style="width: 1.5rem;"></i><span class="ml-3">Lorem ipsum dolor sit.</span>
+                            </div>
+                            <div><i class="fas fa-trophy text-center" style="width: 1.5rem;"></i><span class="ml-3">Lorem ipsum
+                                    dolor sit.</span></div>
+                        </div>
                     </div>
                 </div>
-            <div class="col-md-1"></div>
-        </div>
-        <hr class="lineahoridet">
-
-        <?php
-                require_once 'database/databaseConection.php';
-            
-                $pdo5 = Database::connect(); 
-                $sql5 = "SELECT * FROM modulo WHERE id_curso='$id'";
-                $q5 = $pdo5->prepare($sql5);
-                $q5->execute(array());
-
-                while($dato5 = $q5->fetch(PDO::FETCH_ASSOC)){
-                    ?>
-
-                        
-        <div class="row" >
-        <div class="col-md-1"></div>
-        <div class="col-md-10">
-        <div class="accordion_outerdet">
-            <div class="containerdet">
-                <details>
-                    <summary><?php echo $dato5['nombreModulo'] ?><p>1h 30 min</p></summary>
-                        <div class="infodet">
-                            <?php
-                                 $pdo6 = Database::connect(); 
-                                 $aux1 = $dato5['idModulo'];
-                                 $sql6 = "SELECT * FROM tema WHERE id_modulo='$aux1'";
-                                 $q6 = $pdo6->prepare($sql6);
-                                 $q6->execute(array());
-                                 while($dato6 = $q6->fetch(PDO::FETCH_ASSOC)){
-                            ?>
-                                     <a href=""><?php echo $dato6['nombreTema']  ?></a><bR>
-                                <?php
-                                  }
-                                ?>
-
-                        </div>
-                </details>
-            </div>
             </div>
         </div>
-        <div class="col-md-1"></div>
-        </div>
-
-        <hr class="lineahoridet">
-
-
-                    <?php
-                              
-                }
-                Database::disconnect();
-
-        ?>
-
-       
-
-        <br>
-        <br>
-        <br>
-
     </div>
-<!-- end FAQ -->
-
-</section>
-
-
-
-
-<?php
-    // }
-    // else{
-    //             header('Location:iniciosesion.php');
-    // }
-?>
-
-
+    <div class="bg-light">
+        <div class="row py-5">
+            <div class="col-12 col-sm-12 col-md-5 col-lg-4 col-xl-4 info-course-left" style="border: 1px solid red;">
+                <h4>Contenido del curso</h4>
+            </div>
+            <div class="col-12 col-sm-12 col-md-7 col-lg-8 col-xl-8 text-dark">
+                <h4 class="font-weight-bold">Contenido del curso</h4>
+                <div class="d-flex">
+                    <div class="mr-auto p-2">
+                        <span class="mr-1">47</span>secciones <i class="fas fa-circle mx-2" style="font-size: 5px;"></i>
+                        <span class="mr-1">313</span>secciones <i class="fas fa-circle mx-2" style="font-size: 5px;"></i>
+                        <span class="mr-1">30 h 21 m</span>Lorem, ipsum dolor.
+                    </div>
+                    <div class="p-2">
+                        <h6>Ampliar todas las secciones</h6>
+                    </div>
+                </div>
+                <div id="accordion">
+                    <div class="card">
+                        <a class="card-header card-link" data-toggle="collapse" href="#collapseOne">
+                            <span><i class="fas fa-sort-down mr-3"></i> aaaaaaaaaaaaa</span>
+                        </a>
+                        <div id="collapseOne" class="collapse show" data-parent="#accordion">
+                            <div class="card-body">
+                                Lorem ipsum..
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a class="card-header collapsed card-link" data-toggle="collapse" href="#collapseTwo">
+                            <span><i class="fas fa-sort-down mr-3"></i> aaaaaaaaaaaaa</span>
+                        </a>
+                        <div id="collapseTwo" class="collapse" data-parent="#accordion">
+                            <div class="card-body">
+                                Lorem ipsum..
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a class="card-header collapsed card-link" data-toggle="collapse" href="#collapseThree">
+                            <span><i class="fas fa-sort-down mr-3"></i> aaaaaaaaaaaaa</span>
+                        </a>
+                        <div id="collapseThree" class="collapse" data-parent="#accordion">
+                            <div class="card-body">
+                                Lorem ipsum..
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a class="card-header collapsed card-link" data-toggle="collapse" href="#collapsefour">
+                            <span><i class="fas fa-sort-down mr-3"></i> aaaaaaaaaaaaa</span>
+                        </a>
+                        <div id="collapsefour" class="collapse" data-parent="#accordion">
+                            <div class="card-body">
+                                Lorem ipsum..
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a class="card-header collapsed card-link" data-toggle="collapse" href="#collapsefifth">
+                            <span><i class="fas fa-sort-down mr-3"></i> aaaaaaaaaaaaa</span>
+                        </a>
+                        <div id="collapsefifth" class="collapse" data-parent="#accordion">
+                            <div class="card-body">
+                                Lorem ipsum..
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
