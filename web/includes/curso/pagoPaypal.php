53aae2d886e3118f25bd499512b3253d21870f0d
sheyla2112 <sheylinrey21@gmail.com>
2021-12-29 21:45:40 -0500

    proceso de pago, Reymundez S

--- web/includes/curso/pagoPaypal.php
+++ web/includes/curso/pagoPaypal.php
@@ -1,135 +1,136 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php
-    require_once 'database/databaseConection.php';
+require_once 'database/databaseConection.php';
 ?>
+
 <head>
-    <title>Pagina Pago</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/pagepay.css" />
 </head>
 
 <body>
-
-    <?php
-        $id = $_GET['id'];
-        $pdo = Database::connect();
-        $sql = "SELECT * FROM cursos WHERE idCurso='$id'";
-        $q = $pdo->prepare($sql);
-        $q->execute(array());
-        $dato = $q->fetch(PDO::FETCH_ASSOC);
-        
-
-        $idUserr = $_SESSION['codUsuario'];
-        $veriS="SELECT * FROM cursoinscrito WHERE curso_id = $id AND usuario_id='$idUserr'";
-        $qS = $pdo->prepare($veriS);
-        $qS->execute(array());
-        $datoS=$qS->fetch(PDO::FETCH_ASSOC);
-        Database::disconnect();
-
-        if (empty($datoS['id_cursoInscrito'])){        
-    ?>
-    
-
-    <div class="container">
-        <div class="contenedor-nextpage">
-            <img class="image-cont" src="assets/images/online-payment.png" alt="">
-            <h1 class="mt-4">¡Paso Final!</h1>
-            <hr>
-            <h3>Estas a punto de pagar con paypal la cantidad de:</h3>
-            <div class="my-3 mx-auto" style="max-width: 350px;">
-                <span>$.<?php echo $dato['costoCurso'];?>.00</span><br>
-                <div class> <!-- VISA -->
-                    <a href="" class="btn my-1 visa"><img src="assets/images/visa.png" alt="" style="border-radius: 23px;"></a><br>
-                    <!-- <a href="" class="btn my-1 paypal"><img src="assets/images/paypal (1).png" alt=""></a><br>
-                    <a href="" class="btn my-1 credit"><i class="fas fa-credit-card mr-2"></i>Debit or Credit Card</a><br><br> -->
-                </div>
-                <div id="paypal-button-container"></div>
-                <h5>La forma rápida y segura de pagar</h5>
+    <header style="background-color: #ffffff77;">
+        <div class="container-header navbar-fixed-top" style="max-width: 95rem;">
+            <input type="checkbox" name="" id="check">
+            <div class="logo-container">
+                <a href="index.php"><img src="assets/images/Logo.svg" alt=""></a>
             </div>
-            <h4>Los productos podrán ser descargados una vez que se procede el pago</h4>
-            <h5>(Para aclaraciones :uputoamoestuvoaqui@gmail.com)</h5>
+            <div class="nav-btn-header">
+                <div class="nav-links-header">
+                </div>
+                <div class="log-sign" style="--i: 1.8s">
+                    <ul>
+                        <li class="nav-link" style="--i: .6s">
+                            <a href="#">Cerrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="hamburger-menu-container">
+                <div class="hamburger-menu">
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container pt-5">
+        <div class="row pt-5">
+            <div class="col-12 mb-3 container-navbar-router">
+                <a href="#">Inicio</a><i class=" mx-3 fas fa-chevron-right"></i>
+                <span>Proceso de pago</span>
+            </div>
+            <!-- col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 -->
+            <div class="col-12 col-sm-12 col-md-7 col-lg-8 col-xl-8 mb-0">
+                <form action="" style="height: 100%;">
+                    <div class="container-pay px-4" style="background-color: white;height: 100%;">
+                        <div class="d-flex justify-content-between">
+                            <div class="mt-3">
+                                <h4>Elige tu medio de pago</h4>
+                            </div>
+                            <div class="mt-2">
+                                <span><a href=" #">¿Necesitas ayuda?</a></span>
+                            </div>
+                        </div>
+                        <div class="container-pay-online my-3">
+                            <div class="col-12">
+                                <div class="py-3">
+                                    <i class="far fa-credit-card mr-3"></i>Pago online
+                                </div>
+                            </div>
+                        </div>
+                        <div class="container-pay-card my-3">
+                            <a href="#">
+                                <div class="col-12 my-2">
+                                    <div class="py-3">
+                                        <i class="far fa-dot-circle mr-2"></i>Tarjeta <img src="assets/images/visa.png" alt="" height="13px">
+                                        <!-- <div></div> -->
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="container-pay-card my-3">
+                            <div class="col-12 my-2">
+                                <div class="py-3">
+                                    <div class="form-check">
+                                        <label class="form-check-label">
+                                            <input type="radio" class="form-check-input" name="optradio">Tarjeta <img class="ml-1" src="assets/images/paypal (1).png" alt="" height="13px">
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="container-pay-card my-3">
+                            <div class="col-12 my-2">
+                                <div class="py-3">
+                                    <div class="form-check">
+                                        <label class="form-check-label">
+                                            <input type="radio" class="form-check-input" name="optradio">Tarjeta de débito
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-12 col-sm-12 col-md-5 col-lg-4 col-xl-4">
+                <div class="container-info-course" style="background-color: white;">
+                    <div class="px-4 py-2">
+                        <h5>Detalles del pedido</h5>
+                        <!-- <p class="m-0">Curso:</p> -->
+                        <div class="d-flex flex-column">
+                            <span>Nombre del curso</span>
+                            <span>Descripción del curso</span>
+                            <span>Nombre del curso</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-info-resumen mt-3" style="background-color: white;">
+                    <div class="px-4 py-3">
+                        <h5>Resumen de pedido</h5>
+                        <!-- <p class="m-0">Curso:</p> -->
+                        <div class="row">
+                            <div class="col-6">Producto</div>
+                            <div class="col-6 text-right"><span>s/99.90</span></div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-5 col-lg-6 col-xl-5">
+                                <div class="container-image-detalle" style="width:100px; height:100px">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAPars5s2FQzySbkUpPjtBlvPlANAFDLP7x38q8nOqcke_Lrf_if34Y-kTjGQgS6pRvuQ&usqp=CAU" alt="">
+                                </div>
+                            </div>
+                            <div class="col-7 col-lg-6 col-xl-7 text-leftt"><span>nombre de curso</span>
+                                <p class="font-weight-bold text-danger">s/99.90</p>
+                            </div>
+                        </div>
+                            <hr>
+                        <div class="mt-2">
+                            <h4 style="font-size: 10px;">Los productos podrán ser descargados una vez que se procede el pago</h4>
+                            <h5 class="font-weight-bold" style="font-size: 8px;">(Para aclaraciones giancarlosuggardaddy@gmail.com)</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-    <?php
-        }else{
-            echo'
-                <script>
-                    window.location = "../../curso.php?id='.$id.'";
-                    
-                </script>
-            ';
-        }
-    ?>
-
-    <!-- CLIENTE REAL 
-    <script src="https://www.paypal.com/sdk/js?client-id=AbnJTS6i2adyvJS6ZQxGXFyk7aAsytmqwwOAFy-SEHVZ39rHIfC6LUOf8B9o-y-vd9RkjkdgCNVfGNBC&currency=USD" data-sdk-integration-source="button-factory"></script> -->
-                
-    <!-- SANDBOX -->
-    <script src="https://www.paypal.com/sdk/js?client-id=AVnkZnDaKvFAocz7KIUYvfvpw4DcrqR5DK0dMdD4-BaisXfbd0eKi2qG2hBDv5wkLbc52alNaMqW4s3j&currency=USD" data-sdk-integration-source="button-factory"></script> 
-
-    <script>
-        paypal.Buttons({
-
-            style: {
-                color: 'gold',
-                shape: 'pill',
-                label: 'pay'
-            },
-
-            createOrder: function(data, actions) {
-                return actions.order.create({
-                    purchase_units: [{
-                        amount: {
-                            value: <?php echo $dato['costoCurso'];?>
-                        }
-                        
-                    }]
-                });
-            },
-            onApprove: function(data, actions) {
-                
-                // actions.order.capture().then(function(details){
-                //     alert('pago exitoso');
-                //     window.location.href = 'sidebarCursos.php';
-                //     console.log(details);
-                // });
-                // let URL = 'includes/Cursos_crud/inscribeteCurso.php?id=?php echo $dato["idCurso"]; ?>';
-                let url = 'includes/Cursos_crud/inscribeteCurso.php?id=<?php echo $dato["idCurso"];?>';
-                actions.order.capture().then(function(details) {
-                    alert('pago exitoso');
-                    //     window.location.href = 'sidebarCursos.php';
-                    window.location = "../web/curso.php?id= <?php echo $dato["idCurso"];?>";
-                    console.log(details);
-                    
-                    return fetch(url, {
-                        method: 'post',
-                        headers: {
-                            'content-type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            details: details
-                        })
-                    })
-
-                });
-            },
-
-            onCancel: function(data) {
-                alert('cancelaste tu pago');
-                console.log(data);
-            }
-
-
-        }).render('#paypal-button-container');
-    </script>
 </body>
 
 </html>
