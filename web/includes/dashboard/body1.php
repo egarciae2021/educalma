7015312af74a015f3c9e02094b32243bb07aa820
sheyla2112 <sheylinrey21@gmail.com>
2022-01-05 19:14:21 -0500

    ajuste de sidebar dashboard, Reymundez S

--- web/includes/dashboard/body1.php
+++ web/includes/dashboard/body1.php
@@ -5,23 +5,23 @@
      
 ?>
  <!--========== NAV ==========-->
- <div class="nav" id="navbar">
-     <nav class="nav__container">
+ <div class="nav-dashboard" id="navbar">
+     <nav class="nav__container-dashboard">
          <div>
-             <a href="index.php" class="nav__link nav__logo">
-                 <img src="./assets/images/imagen_2021-10-28_233347.png" alt="" class="header__img__logo">
-                 <span class="nav__logo-name"> Educalma</span>
+             <a href="index.php" class="nav__link-dashboard nav__logo-dashboard">
+                 <img src="./assets/images/imagen_2021-10-28_233347.png" alt="" class="header__img__logo-dashboard">
+                 <span class="nav__logo-name-dashboard"> Educalma</span>
                  
                  <!-- <i style="margin-left: 9rem; display: none;" class='bx bx-menu' id="header-toggle"></i> -->
              </a>
-             <div class="nav__list">
-                 <div class="nav__items">
-                     <h3 class="nav__subtitle__img">
+             <div class="nav__list-dashboard">
+                 <div class="nav__items-dashboard">
+                     <h3 class="nav__subtitle__img-dashboard">
                          
                      <!-- <img src="./assets/images/imagen_2021-10-29_000020.png" alt="" class="header__img__side"></h3> -->
-                     <img src="data:image/*;base64,<?php echo base64_encode($dato['mifoto']); ?>" alt="foto_curso" class="header__img__side"></h3>
+                     <img src="data:image/*;base64,<?php echo base64_encode($dato['mifoto']); ?>" alt="foto_curso" class="header__img__side-dashboard"></h3>
 
-                     <h4 class="nav__subtitle">
+                     <h4 class="nav__subtitle-dashboard">
                          <?php
                             if (isset($_SESSION['Logueado']) && $_SESSION['Logueado'] === true) {
                                 echo $_SESSION['nombres'];
@@ -30,41 +30,41 @@
                             }
                             ?>
                      </h4>
-                     <div class="nav__dropdown">
-                         <a href="#" class="nav__link">
-                             <i class="far fa-user-circle nav__icon"></i>
+                     <div class="nav__dropdown-dashboard">
+                         <a href="#" class="nav__link-dashboard">
+                             <i class="far fa-user-circle nav__icon-dashboard"></i>
                              <!-- <i class="far fa-bookmark nav__icon"></i> -->
-                             <span class="nav__name">Perfil</span>
+                             <span class="nav__name-dashboard">Perfil</span>
                              <!-- <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i> -->
-                             <i class="fas fa-caret-down nav__icon nav__dropdown-icon"></i>
+                             <i class="fas fa-caret-down nav__icon-dashboard nav__dropdown-icon-dashboard"></i>
                          </a>
 
-                         <div class="nav__dropdown-collapse">
-                             <div class="nav__dropdown-content">
-                                 <a href="user-sidebar.php" class="nav__dropdown-item nav__link">Dashboard</a>
+                         <div class="nav__dropdown-collapse-dashboard">
+                             <div class="nav__dropdown-content-dashboard">
+                                 <a href="user-sidebar.php" class="nav__dropdown-item-dashboard nav__link-dashboard">Dashboard</a>
                              </div>
                          </div>
                      </div>
 
 
-                     <div class="nav__dropdown">
-                         <a href="sidebarCursos.php" class="nav__link">
-                             <i class="far fa-folder nav__icon"></i>
+                     <div class="nav__dropdown-dashboard">
+                         <a href="sidebarCursos.php" class="nav__link-dashboard">
+                             <i class="far fa-folder nav__icon-dashboard"></i>
                              <!-- <i class="far fa-bookmark nav__icon"></i> -->
-                             <span class="nav__name">Cursos</span>
+                             <span class="nav__name-dashboard">Cursos</span>
                              <!-- <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i> -->
-                             <i class="fas fa-caret-down nav__icon nav__dropdown-icon"></i>
+                             <i class="fas fa-caret-down nav__icon-dashboard nav__dropdown-icon-dashboard"></i>
                          </a>
 
-                         <div class="nav__dropdown-collapse">
-                             <div class="nav__dropdown-content">
-                                 <a href="sidebarCursos.php" class="nav__dropdown-item nav__link">Mis Cursos</a>
-                                 <a href="ListaCursos.php?pag=1" class="nav__dropdown-item nav__link">Ver todos los Cursos</a>
+                         <div class="nav__dropdown-collapse-dashboard">
+                             <div class="nav__dropdown-content-dashboard">
+                                 <a href="sidebarCursos.php" class="nav__dropdown-item-dashboard nav__link-dashboard">Mis Cursos</a>
+                                 <a href="ListaCursos.php?pag=1" class="nav__dropdown-item-dashboard nav__link-dashboard">Ver todos los Cursos</a>
                                  <?php
                                     if($_SESSION['privilegio']==1 ||$_SESSION['privilegio']==6 ){
                                 ?>
-                                 <a href="agregarcurso.php" class="nav__dropdown-item nav__link">Donar Curso</a>
-                                 <a href="publicarcursos.php?pag=1" class="nav__dropdown-item nav__link">Publicar cursos</a>
+                                 <a href="agregarcurso.php" class="nav__dropdown-item-dashboard nav__link-dashboard">Donar Curso</a>
+                                 <a href="publicarcursos.php?pag=1" class="nav__dropdown-item-dashboard nav__link-dashboard">Publicar cursos</a>
                                  <?php
                                   }
                                 ?>
@@ -75,18 +75,18 @@
                     <?php
                         if($_SESSION['privilegio']==1 ||$_SESSION['privilegio']==6 ){
                     ?>
-                        <div class="nav__dropdown">
-                            <a href="agregarCategorias.php" class="nav__link">
-                                <i class="far fa-check-circle nav__icon"></i>
-                                <span class="nav__name">Categoria</span>
+                        <div class="nav__dropdown-dashboard">
+                            <a href="agregarCategorias.php" class="nav__link-dashboard">
+                                <i class="far fa-check-circle nav__icon-dashboard"></i>
+                                <span class="nav__name-dashboard">Categoria</span>
                                 <!-- <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i> -->
-                                <i class="fas fa-caret-down nav__icon nav__dropdown-icon"></i>
+                                <i class="fas fa-caret-down nav__icon-dashboard nav__dropdown-icon-dashboard"></i>
                             </a>
 
-                            <div class="nav__dropdown-collapse">
-                                <div class="nav__dropdown-content">
+                            <div class="nav__dropdown-collapse-dashboard">
+                                <div class="nav__dropdown-content-dashboard">
                                     
-                                    <a href="agregarCategorias.php" class="nav__dropdown-item nav__link">Nueva Categoria</a>
+                                    <a href="agregarCategorias.php" class="nav__dropdown-item-dashboard nav__link-dashboard">Nueva Categoria</a>
                                     
                                 </div>
                             </div>
@@ -96,25 +96,25 @@
                         }
                     ?>
 
-                     <a href="index.php" class="nav__link">
+                     <a href="index.php" class="nav__link-dashboard">
                          <!-- <i class='bx bx-home nav__icon'></i> -->
-                         <i class="fas fa-home nav__icon"></i>
-                         <span class="nav__name">Educalma</span>
+                         <i class="fas fa-home nav__icon-dashboard"></i>
+                         <span class="nav__name-dashboard">Educalma</span>
                      </a>
 
-                     <a href="sidebarEditar.php" class="nav__link">
-                         <i class="fas fa-cog nav__icon"></i>
-                         <span class="nav__name">Ajustes</span>
+                     <a href="sidebarEditar.php" class="nav__link-dashboard">
+                         <i class="fas fa-cog nav__icon-dashboard"></i>
+                         <span class="nav__name-dashboard">Ajustes</span>
                      </a>
                      <!-- <a href="#" class="nav__link">
                          <i class="fas fa-question-circle nav__icon"></i>
                          <span class="nav__name">Ayuda</span>
                      </a> -->
 
-                     <a href="includes/login/logout.php" class="nav__link nav__logout">
+                     <a href="includes/login/logout.php" class="nav__link nav__logout-dashboard">
                          <!-- <i class='bx bx-log-out nav__icon'></i> -->
-                         <i class="fas fa-sign-out-alt nav__icon"></i>
-                         <span class="nav__name">Salir</span>
+                         <i class="fas fa-sign-out-alt nav__icon-dashboard"></i>
+                         <span class="nav__name-dashboard">Salir</span>
                      </a>
 
                  </div>
--- web/includes/dashboard/head1.php
+++ web/includes/dashboard/head1.php
@@ -12,6 +12,34 @@
     <link rel="stylesheet" href="assets/css/style4.css">
     <link rel="stylesheet" href="assets/css/stymiscursos.css">
 
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css" />
+    <!-- Pogo Slider CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/pogo-slider.min.css" />
+    <!-- Site CSS -->
+    <!-- <link rel="stylesheet" href="assets/css/style.css" />  -->
+    <!-- Preloader style -->
+    <!-- <link rel="stylesheet" href="assets/css/preloader.css" />  -->
+    <!-- <link rel="stylesheet" href="assets/css/header.css" />  -->
+    <!-- <link rel="stylesheet" href="assets/css/footer.css" />  -->
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/responsive.css" />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/custom.css" />
+
+<!-- ADMINLTE3 -->
+     <!-- Google Font: Source Sans Pro -->
+     <!-- Font Awesome --> 
+     <!-- <link rel="stylesheet" href="includes/plugins/fontawesome-free/css/all.min.css"> -->
+    
+    <link rel="stylesheet" href="includes/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="includes/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="includes/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <!-- <link rel="stylesheet" href="../../main.css"> -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.js"></script>
+
     <title>Educalma Dashboard</title>
 </head>
 <?php
