f8af29192e92fec38c295aa2f6a3fb188a901fbc
gfsp2001 <gfsp2001@hotmail.com>
2022-02-01 10:10:01 -0500

    .

--- web/database/EduCalma.sql
+++ web/database/EduCalma.sql
@@ -21,7 +21,7 @@ CREATE TABLE usuarios (
 	cod_tipoDonador int(10),
 	estado boolean not null, -- 0 = Inactivo, 1 = Activo
 	fecha_registro datetime not null,
-	mifoto longblob,
+	mifoto longblob DEFAULT NULL,
 	token_password varchar(100) DEFAULT NULL,
 	password_request int(11) NOT NULL DEFAULT '0',
 	cod_Emp varchar(10) DEFAULT NULL
@@ -30,22 +30,22 @@ CREATE TABLE usuarios (
 SELECT * FROM usuarios;
 -- USUARIO 
 -- pass: July123456
-INSERT INTO usuarios VALUES (1, 3, 0, 0, 'JULY', 'CASTILLO', 'BARRERA','j@gmail.com', '$2y$10$eC0Uu2bWV/Gqm1rjuB3oL.DnA8FaDkLpjF/.Wi8604GaLu1aN2/xG', '965478456', 1, '89475632', 2, '1992-07-28', 'PERÚ', 1, 1, now(), null, '', 0, null);
+INSERT INTO usuarios VALUES (1, 3, 0, 0, 'JULY', 'CASTILLO', 'BARRERA', 'j@gmail.com', '$2y$10$eC0Uu2bWV/Gqm1rjuB3oL.DnA8FaDkLpjF/.Wi8604GaLu1aN2/xG', '965478456', 1, '89475632', 2, '1992-07-28', 'PERÚ', 1, 1, '2021-11-09 19:25:45', NULL, '', 0, '');
 -- ADMINISTRADOR 
 -- pass: Julio123456
-INSERT INTO usuarios VALUES (2, 1, 0, 0, 'JULIO', 'CESAR', 'TELLO','tello@gmail.com', '$2y$10$tyWFJ1bWHdciAaxbGAk7uu.HIPoZw3uDKJJ4Rv7914wh6Hai9iroi', '954555784', 1, '55784952', 1, '1989-04-02', 'PERÚ', 1, 1, now(), null, '', 0, null);
+INSERT INTO usuarios VALUES (2, 1, 0, 0, 'JULIO', 'CESAR', 'TELLO', 'tello@gmail.com', '$2y$10$tyWFJ1bWHdciAaxbGAk7uu.HIPoZw3uDKJJ4Rv7914wh6Hai9iroi', '954555784', 1, '55784952', 1, '1989-04-02', 'PERÚ', 1, 1, '2021-11-09 19:25:45', NULL, '', 0, '');
 --  USUARIO
 -- pass: Marleni123456
-INSERT INTO usuarios VALUES (3, 3, 0, 0, 'MARLENE', 'VASQUEZ', 'VERA','marlene@gmail.com', '$2y$10$apc7Qxi7dW5FRo0Ki9BOue2FRp/yfBWhiCeVnRAQGoNgxkMpvlMhy', '989874852', 1, '05554874', 2, '1980-03-18', 'PERÚ', 3, 1, now(), null, '', 0, null);
--- USUARIO 
--- pass: Gloria123456
-INSERT INTO usuarios VALUES (4, 3, 1, 0, 'GLORIA', 'SAC', 'LECHE','gloria@gloria.com', '$2y$10$4X1m7wnrbbuuOvqYRkd7gu26Xik2WpYZVQ/OVFvRC05gEp13yFQSa', '999874520', 4, '10554878920', 5, '1900-03-18', 'PERÚ', 1, 1, now(), null, '', 0, null);
+INSERT INTO usuarios VALUES (3, 3, 0, 0, 'MARLENE', 'VASQUEZ', 'VERA', 'marlene@gmail.com', '$2y$10$apc7Qxi7dW5FRo0Ki9BOue2FRp/yfBWhiCeVnRAQGoNgxkMpvlMhy', '989874852', 1, '05554874', 2, '1980-03-18', 'PERÚ', 3, 1, '2021-11-09 19:25:45', NULL, '', 0, ''),
 -- ADMINISTRADOR 
 -- pass: Estefany123456
-INSERT INTO usuarios VALUES (5, 1, 0, 1, 'ESTEFANY', 'MATA', 'MATOS','estefany@gloria.com', '$2y$10$OGIZkqniYZsz1HQfcKdzfuNa0pyjYFGPB1K71uXrigk4ITw5xFJxW', '963259658', 1, '02698974', 2, '1990-01-05', 'PERÚ', 1, 1, now(), null, '', 0, null);
+INSERT INTO usuarios VALUES (4, 1, 0, 1, 'ESTEFANY', 'MATA', 'MATOS', 'estefany@gloria.com', '$2y$10$OGIZkqniYZsz1HQfcKdzfuNa0pyjYFGPB1K71uXrigk4ITw5xFJxW', '963259658', 1, '02698974', 2, '1990-01-05', 'PERÚ', 1, 1, '2021-11-09 19:25:45', NULL, '', 0, ''),
 -- ADMINISTRADOR 
 -- pass: Calma123
-INSERT INTO usuarios VALUES (6, 1, 0, 1, 'MATA', 'MATA', 'MATOS','educalma@calma.com', '$2y$10$ZCyPSbUMoUa9C2vA3CNfR.caGn8Q2cc3Z1OFgH4rssGJu3JatBVGG', '963259658', 1, '02698974', 2, '1990-01-05', 'PERÚ', 1, 1, now(), null, '', 0, null);
+INSERT INTO usuarios VALUES (5, 1, 0, 1, 'MATA', 'MATA', 'MATOS', 'educalma@calma.com', '$2y$10$ZCyPSbUMoUa9C2vA3CNfR.caGn8Q2cc3Z1OFgH4rssGJu3JatBVGG', '963259658', 1, '02698974', 2, '1990-01-05', 'PERÚ', 1, 1, '2021-11-09 19:25:45', NULL, '', 0, '');
+-- ADMINISTRADOR
+-- pass: 2587734Santi
+INSERT INTO usuarios VALUES (7, 1, 0, 1, 'Maria Jose', 'Sansa ', 'Martinez', 'prueba132@hotmail.com', '$2y$10$viz7KJcbRFvwmG5FsGoE2.GVSzNCzIUAU7kVomnNQ3fZa6gmQ8wEW', '321456879', 1, '12354896', 1, '2001-05-29', '1', 1, 1, '2022-02-01 10:00:34', '', NULL, 0, NULL);
 
 -- Tabla Privilegio
 CREATE TABLE privilegio (
