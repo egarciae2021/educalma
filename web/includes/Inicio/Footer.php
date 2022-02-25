64f9d9749973547fb57f06a0ebc2e41e7f817149
royerbardales <reyorcristian@gmail.com>
2022-02-25 16:46:34 -0500

    redireccionamos a una nueva ventana los enlace

--- web/includes/Inicio/Footer.php
+++ web/includes/Inicio/Footer.php
@@ -1,76 +1,76 @@
 <footer class="footer--light">
-    <div class="footer-big">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
-            <div class="footer-widget">
-              <div class="widget-about">
-                <img src="assets/images/Logo.svg" alt="" class="img-fluid">
-              </div>
+  <div class="footer-big">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+          <div class="footer-widget">
+            <div class="widget-about">
+              <img src="assets/images/Logo.svg" alt="" class="img-fluid">
             </div>
           </div>
-          <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
-            <div class="footer-widget">
-              <div class="footer-menu">
-                <h4 class="footer-widget-title">Nosotros</h4>
-                <ul>
-                  <li>
-                    <a href="https://fundacioncalma.org/%C2%BFqui%C3%A9nes-somos%3F">Qui&eacute;nes Somos</a>
-                  </li>
-                  <li>
-                    <a href="https://fundacioncalma.org/contacto">Cont&aacute;ctanos</a>
-                  </li>
-                  <li>
-                    <a href="https://fundacioncalma.org/%C2%BFc%C3%B3mo-ayudo%3F">Ay&uacute;danos</a>
-                  </li>
-                </ul>
-              </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+          <div class="footer-widget">
+            <div class="footer-menu">
+              <h4 class="footer-widget-title">Nosotros</h4>
+              <ul>
+                <li>
+                  <a href="https://fundacioncalma.org/%C2%BFqui%C3%A9nes-somos%3F">Qui&eacute;nes Somos</a>
+                </li>
+                <li>
+                  <a href="https://fundacioncalma.org/contacto">Cont&aacute;ctanos</a>
+                </li>
+                <li>
+                  <a href="https://fundacioncalma.org/%C2%BFc%C3%B3mo-ayudo%3F">Ay&uacute;danos</a>
+                </li>
+              </ul>
             </div>
           </div>
-          <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
-            <div class="footer-widget">
-              <div class="footer-menu">
-                <!-- <h4 class="footer-widget-title">Blog</h4> -->
-                <b><a href="https://fundacioncalma.org/" >Blog</a></b> 
-                <ul>
-                  <li>
-                    <a href="certi.php">Validaci&oacute;n de Certificado</a>
-                  </li>
-                  <li>
-                    <!-- <a href="recursos1.php">Presentaci&oacute;n</a> -->
-                    <a href="recursos1.php">Comparte tu curso</a>
-                  </li>
-                </ul>
-              </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+          <div class="footer-widget">
+            <div class="footer-menu">
+              <!-- <h4 class="footer-widget-title">Blog</h4> -->
+              <b><a href="https://fundacioncalma.org/">Blog</a></b>
+              <ul>
+                <li>
+                  <a href="certi.php">Validaci&oacute;n de Certificado</a>
+                </li>
+                <li>
+                  <!-- <a href="recursos1.php">Presentaci&oacute;n</a> -->
+                  <a href="recursos1.php">Comparte tu curso</a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="site-footer">
-      <div class="container">
-        <hr class="small">
-      </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-12">
-            <p class="copyright-text">Copyright © 2021 All Rights Reserved by
-              <a href="#">EduCalma</a>
-            </p>
-          </div>
+  </div>
+  <div class="site-footer">
+    <div class="container">
+      <hr class="small">
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-sm-6 col-12">
+          <p class="copyright-text">Copyright © 2021 All Rights Reserved by
+            <a href="#">EduCalma</a>
+          </p>
+        </div>
 
-          <div class="col-md-4 col-sm-6 col-12">
-            <ul class="social-icons">
-              <li><a class="facebook" href="https://www.facebook.com/fundacioncalma.org/"><i class="fab fa-facebook-f"></i></a></li>
-              <li><a class="twitter" href="https://www.youtube.com/channel/UCsEmW0is_Q-d_IMfux0pfEw"><i class="fab fa-twitter"></i></a></li>
-              <li><a class="instagram" href="https://www.instagram.com/fundacioncalma/?hl=es"><i class="fab fa-instagram"></i></a></li>
-              <li><a class="whatsapp" href="https://api.whatsapp.com/send?phone=51910571087&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20los%20cursos%20."><i class="fab fa-whatsapp"></i></a></li>
-              <li><a class="envelope" href="mailto:contacto@fundacioncalma.org"><i class="far fa-envelope"></i></i></a></li>
-            </ul>
-          </div>
+        <div class="col-md-4 col-sm-6 col-12">
+          <ul class="social-icons">
+            <li><a target="_blank" class="facebook" href="https://www.facebook.com/fundacioncalma.org/"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a target="_blank" class="twitter" href="https://www.youtube.com/channel/UCsEmW0is_Q-d_IMfux0pfEw"><i class="fab fa-twitter"></i></a></li>
+            <li><a target="_blank" class="instagram" href="https://www.instagram.com/fundacioncalma/?hl=es"><i class="fab fa-instagram"></i></a></li>
+            <li><a target="_blank" class="whatsapp" href="https://api.whatsapp.com/send?phone=51910571087&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20los%20cursos%20."><i class="fab fa-whatsapp"></i></a></li>
+            <li><a class="envelope" href="mailto:contacto@fundacioncalma.org"><i class="far fa-envelope"></i></i></a></li>
+          </ul>
         </div>
       </div>
-  </footer>
+    </div>
+</footer>
 
 
 
@@ -134,106 +134,106 @@
 
 
 <script>
-/* counter js */
+  /* counter js */
 
-(function($) {
+  (function($) {
     $.fn.countTo = function(options) {
-        options = options || {};
+      options = options || {};
 
-        return $(this).each(function() {
-            // set options for current element
-            var settings = $.extend({}, $.fn.countTo.defaults, {
-                from: $(this).data('from'),
-                to: $(this).data('to'),
-                speed: $(this).data('speed'),
-                refreshInterval: $(this).data('refresh-interval'),
-                decimals: $(this).data('decimals')
-            }, options);
+      return $(this).each(function() {
+        // set options for current element
+        var settings = $.extend({}, $.fn.countTo.defaults, {
+          from: $(this).data('from'),
+          to: $(this).data('to'),
+          speed: $(this).data('speed'),
+          refreshInterval: $(this).data('refresh-interval'),
+          decimals: $(this).data('decimals')
+        }, options);
 
-            // how many times to update the value, and how much to increment the value on each update
-            var loops = Math.ceil(settings.speed / settings.refreshInterval),
-                increment = (settings.to - settings.from) / loops;
+        // how many times to update the value, and how much to increment the value on each update
+        var loops = Math.ceil(settings.speed / settings.refreshInterval),
+          increment = (settings.to - settings.from) / loops;
 
-            // references & variables that will change with each update
-            var self = this,
-                $self = $(this),
-                loopCount = 0,
-                value = settings.from,
-                data = $self.data('countTo') || {};
+        // references & variables that will change with each update
+        var self = this,
+          $self = $(this),
+          loopCount = 0,
+          value = settings.from,
+          data = $self.data('countTo') || {};
 
-            $self.data('countTo', data);
+        $self.data('countTo', data);
 
-            // if an existing interval can be found, clear it first
-            if (data.interval) {
-                clearInterval(data.interval);
+        // if an existing interval can be found, clear it first
+        if (data.interval) {
+          clearInterval(data.interval);
+        }
+        data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+        // initialize the element with the starting value
+        render(value);
+
+        function updateTimer() {
+          value += increment;
+          loopCount++;
+
+          render(value);
+
+          if (typeof(settings.onUpdate) == 'function') {
+            settings.onUpdate.call(self, value);
+          }
+
+          if (loopCount >= loops) {
+            // remove the interval
+            $self.removeData('countTo');
+            clearInterval(data.interval);
+            value = settings.to;
+
+            if (typeof(settings.onComplete) == 'function') {
+              settings.onComplete.call(self, value);
             }
-            data.interval = setInterval(updateTimer, settings.refreshInterval);
+          }
+        }
 
-            // initialize the element with the starting value
-            render(value);
-
-            function updateTimer() {
-                value += increment;
-                loopCount++;
-
-                render(value);
-
-                if (typeof(settings.onUpdate) == 'function') {
-                    settings.onUpdate.call(self, value);
-                }
-
-                if (loopCount >= loops) {
-                    // remove the interval
-                    $self.removeData('countTo');
-                    clearInterval(data.interval);
-                    value = settings.to;
-
-                    if (typeof(settings.onComplete) == 'function') {
-                        settings.onComplete.call(self, value);
-                    }
-                }
-            }
-
-            function render(value) {
-                var formattedValue = settings.formatter.call(self, value, settings);
-                $self.html(formattedValue);
-            }
-        });
+        function render(value) {
+          var formattedValue = settings.formatter.call(self, value, settings);
+          $self.html(formattedValue);
+        }
+      });
     };
 
     $.fn.countTo.defaults = {
-        from: 0, // the number the element should start at
-        to: 0, // the number the element should end at
-        speed: 1000, // how long it should take to count between the target numbers
-        refreshInterval: 100, // how often the element should be updated
-        decimals: 0, // the number of decimal places to show
-        formatter: formatter, // handler for formatting the value before rendering
-        onUpdate: null, // callback method for every time the element is updated
-        onComplete: null // callback method for when the element finishes updating
+      from: 0, // the number the element should start at
+      to: 0, // the number the element should end at
+      speed: 1000, // how long it should take to count between the target numbers
+      refreshInterval: 100, // how often the element should be updated
+      decimals: 0, // the number of decimal places to show
+      formatter: formatter, // handler for formatting the value before rendering
+      onUpdate: null, // callback method for every time the element is updated
+      onComplete: null // callback method for when the element finishes updating
     };
 
     function formatter(value, settings) {
-        return value.toFixed(settings.decimals);
+      return value.toFixed(settings.decimals);
     }
-}(jQuery));
+  }(jQuery));
 
-jQuery(function($) {
+  jQuery(function($) {
     // custom formatting example
     $('.count-number').data('countToOptions', {
-        formatter: function(value, options) {
-            return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
-        }
+      formatter: function(value, options) {
+        return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+      }
     });
 
     // start all the timers
     $('.timer').each(count);
 
     function count(options) {
-        var $this = $(this);
-        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-        $this.countTo(options);
+      var $this = $(this);
+      options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+      $this.countTo(options);
     }
-});
+  });
 </script>
 </body>
 
