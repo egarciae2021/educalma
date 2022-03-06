f13cda72c8f59e969e066a6ce8b0720315fa9689
admi <jeanzz.1910@gmail.com>
2022-03-05 20:20:09 -0500

    failed to open - Reinol

--- web/Cursoiniciar.php
+++ web/Cursoiniciar.php
@@ -6,7 +6,7 @@
 <?php include_once 'includes/Inicio/Head.php' ?>
 
 <body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
-<?php include_once 'includes/Inicio/loader.php' ?>
+<?php include_once 'includes/Inicio/Loader.php' ?>
 <?php include_once 'includes/Inicio/Header.php' ?>
 
 <?php include_once 'includes/curso/Iniciarcurso.php' ?>
