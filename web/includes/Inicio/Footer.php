1219a0295fbcf4eb264832204cdf59f2a89f2d2f
unknown <gonzalocherre@gmail.com>
2022-03-13 16:41:41 -0500

    cambio de color al foooter y ajustes,Gonzalo Cherre

--- web/includes/Inicio/Footer.php
+++ web/includes/Inicio/Footer.php
@@ -1,4 +1,4 @@
-<footer class="footer--light">
+<footer class="footer--light "style=" background-color: #dcecfa">
     <div class="footer-big">
       <div class="container">
         <div class="row">
@@ -69,7 +69,7 @@
               <li><a target="_blank" class="instagram" href="https://www.instagram.com/fundacioncalma/?hl=es"><i class="fab fa-instagram"></i></a></li>
               <li><a target="_blank" class="whatsapp" href="https://api.whatsapp.com/send?phone=51910571087&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20los%20cursos%20."><i class="fab fa-whatsapp"></i></a></li>
               <li><a target="_blank" class="envelope" href="mailto:contacto@fundacioncalma.org"><i class="far fa-envelope"></i></i></a></li>
-            </ul>
+              </ul>
           </div>
         </div>
       </div>
