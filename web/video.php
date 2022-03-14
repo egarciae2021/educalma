94418c713629f1f4bd6024b906ded56033546c0a
egarciae2021 <73246932@certus.edu.pe>
2022-03-14 13:22:40 -0500

    Actualizar redireccion correo gmail y warning de enlaces Inicio, Edgar

--- web/video.php
+++ web/video.php
@@ -2,14 +2,14 @@
     ob_start();
     @session_start();
 ?>
-<?php include_once 'includes/inicio/Head.php' ?>
+<?php include_once 'includes/Inicio/Head.php' ?>
 
 <body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
-    <?php //include_once 'includes/inicio/loader.php' ?>
-    <?php include_once 'includes/inicio/Header.php' ?>
+    <?php //include_once 'includes/Inicio/loader.php' ?>
+    <?php include_once 'includes/Inicio/Header.php' ?>
 
     <?php include_once 'includes/curso/videoCurso.php' ?>
-    <?php include_once 'includes/inicio/Footer.php' ?>
+    <?php include_once 'includes/Inicio/Footer.php' ?>
     <script src="assets/js/home.js"></script>
 
 </body>
