f5332e707769bd885fb6c9eb2e5b0a9863f12e95
Reynol <reinolleonlaurente@gmail.com>
2022-04-09 14:05:19 -0500

    updtae en comprar Curso - leon

--- web/includes/Cursos_crud/inscribirseGratis.php
+++ web/includes/Cursos_crud/inscribirseGratis.php
@@ -15,23 +15,24 @@ session_start();
     $qS = $pdo->prepare($veriS);
     $qS->execute(array());
     $datoS=$qS->fetch(PDO::FETCH_ASSOC);
-    if (empty($datoS['id_cursoInscrito'])){
+
         try{
             // $veri="INSERT INTO cursoinscrito (curso_id, usuario_id, cod_curso, curso_obt, cantidad_respuestas) VALUES ($idCurso, $idUser, '', 1, 0)";
-            $veri="INSERT INTO `cursoinscrito` (`curso_id`, `usuario_id`, cod_curso, curso_obt, cantidad_respuestas, nota) VALUES (:idCurso, :idUser, '', 1, 0, 0)";
+            $veri="INSERT INTO `cursoinscrito` (`curso_id`, `usuario_id`, cod_curso, cantidad_respuestas, nota) VALUES (:idCurso, :idUser, 1, 0, 0)";
             $q = $pdo->prepare($veri);
             $q->bindParam(":idCurso", $idCurso, PDO::PARAM_INT);
             $q->bindParam(":idUser", $idUser, PDO::PARAM_INT);
             $q->execute();
+            //echo $e->getMessage();
         }catch(PDOException $e){
             echo $e->getMessage();
         }
         Database::disconnect();
-    }
     echo'
                 <script>
-                    //alert ("Necesita Loguearse");
-                    window.location = "../../curso.php?id='.$idCurso.'";
+                    alert ("Necesita Loguearse");
+                    
+                    //window.location = "../../sidebarCursos.php";
                 </script>
             ';
 ?>
