afa0ef46dfb90c45528e2f76a063f1429a367e38
JesusCRojas <cacharojasjesuseusebio@gmail.com>
2022-05-18 00:00:15 -0500

    Cambio de tamaño del logo cambio de logo en el footer y cambio de credenciales para la conexion con la bd, Jesus Cacha

--- web/database/databaseConection.php
+++ web/database/databaseConection.php
@@ -1,10 +1,16 @@
 <?php
 class Database
 {
+  /*
 	private static $dbName = 'educalma' ;
 	private static $dbHost = 'localhost';
 	private static $dbUsername = 'root';
   private static $dbUserPassword = '';
+  */
+  private static $dbHost = 'mysqleducalma';
+	private static $dbUsername = '994896741';
+  private static $dbUserPassword = 'HVc$DhzpSweC';
+
 // datooooo
 
 	private static $cont  = null;
