0055c7ae56b2bec096c3f113cd9abd2f717102a1
JesusCRojas <cacharojasjesuseusebio@gmail.com>
2022-06-01 16:18:58 -0500

    Modificar el diseño de las páginas para agregar nuevos cursos, nuevo temas y cuestionarios, y la página para editar cursos, y programar los botones ATRAS, Jesus Cacha

--- web/Form_pregun_cuestionario.php
+++ web/Form_pregun_cuestionario.php
@@ -5,15 +5,19 @@
 <?php include_once 'includes/Inicio/Head.php' ?>
 
 <head>
-<link rel="shortcut icon" href="assets/images/logo_edu.png">
+    <link rel="shortcut icon" href="assets/images/logo_edu.png">
 </head>
 
 <body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
-<?php //include_once 'includes/Inicio/Loader.php' ?>
-<?php include_once 'includes/Inicio/Header.php' ?>
 
-<?php include_once 'includes/curso/Preguntas_Cuestionar.php' ?>
-<?php include_once 'includes/Inicio/Footer.php' ?>
-<script src="assets/js/home.js"></script>
+    <?php //include_once 'includes/Inicio/Loader.php' ?>
+    
+    <?php include_once 'includes/Inicio/Header.php' ?>
+
+    <?php include_once 'includes/curso/Preguntas_Cuestionar.php' ?>
+    
+    <?php include_once 'includes/Inicio/Footer.php' ?>
+    
+    <script src="assets/js/home.js"></script>
 
 </body>
