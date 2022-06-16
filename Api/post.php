c789e32bc9c3bd096eebfafa623b9deee71aa141
egarciae2021 <73246932@certus.edu.pe>
2022-06-16 05:28:35 -0500

    Uso de api

--- Api/post.php
+++ Api/post.php
@@ -7,18 +7,37 @@ $dbConn =  connect($db);
  */
 if ($_SERVER['REQUEST_METHOD'] == 'GET')
 {
-   
-      //Mostrar lista de post
-      $sql = $dbConn->prepare("SELECT * FROM usuarios");
-      $sql->execute();
-      $sql->setFetchMode(PDO::FETCH_ASSOC);
-      header("HTTP/1.1 200 OK");
-      echo json_encode( $sql->fetchAll()  );
-      exit();
+
+    $myObj->name = "John";
+    $myObj->age = 30;
+    $myObj->city = "New York";
+    
+    $myJSON = json_encode($myObj);
+    
+ 
+
+  //Mostrar un post
+  $sql = $dbConn->prepare("SELECT * FROM `usuarios`");
+ 
+  $sql->execute(); 
+
+  $userData = array();
+
+    while($row=$sql->fetch(PDO::FETCH_ASSOC)){
+    
+        // $userData['AllUsers'][] = $row;
+        echo json_encode($row);
+    
+    }
+
+
+//    header("HTTP/1.1 200 OK");
+ 
+  exit();
 
 }
-
+ 
 
 //En caso de que ninguna de las opciones anteriores se haya ejecutado
-header("HTTP/1.1 400 Bad Request");
+// header("HTTP/1.1 400 Bad Request");
 ?>
