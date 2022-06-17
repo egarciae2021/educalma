bf877777182a526f2968825e79aca2e44dc58fed
BladimirCC <bladimir_18_10@outlook.com>
2022-06-17 12:10:00 -0500

    Reporte con columnas nuevas, Bladimir Cubas Castillo

--- web/includes/dashboard/reporteUsuario3.php
+++ web/includes/dashboard/reporteUsuario3.php
@@ -168,6 +168,8 @@ if (isset($_SESSION['Logueado']) && ($_SESSION['Logueado'] === true)) {
                                         <th style="border-radius: 10px 0 0 10px;">ID Usuario</th>
                                         <th scope="col">Nombre Completo</th>
                                         <th>Curso</th>
+                                        <th>Fecha de Inscripción</th>
+                                        <th>Fecha de Finalización</th>
                                         <th>Avance</th>
                                         <th>Nota</th>
                                     </tr>
@@ -181,7 +183,9 @@ if (isset($_SESSION['Logueado']) && ($_SESSION['Logueado'] === true)) {
                                       echo '
                                           <td>'. $aprobados['idUsuario'].'</td>
                                           <td>'. $aprobados['nombres'].'</td> 
-                                          <td>'. $aprobados['nombreCurso'].'</td> 
+                                          <td>'. $aprobados['nombreCurso'].'</td>
+                                          <td>'. $aprobados['fechaInscripcion'].'</td> 
+                                          <td>'. $aprobados['fechaFinalizacion'].'</td>  
                                           <td>'. $aprobados['avance'].'</td>
                                           <td>'. $aprobados['nota'].'</td>
                                       ';
