9e2d354aaa340cc0ca226eec380b61d8298009eb
egarciae2021 <73246932@certus.edu.pe>
2022-06-17 19:05:48 -0500

    Actualizador api

--- web/Api/post.php
+++ web/Api/post.php
@@ -17,20 +17,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
  
 
   //Mostrar un post
-  $sql = $dbConn->prepare("SELECT * FROM `usuarios`");
+  $sql = $dbConn->prepare("SELECT * FROM `temp` ");
  
   $sql->execute(); 
 
   $userData = array();
 
-    // while($row=$sql->fetch(PDO::FETCH_ASSOC)){ 
-    //     echo json_encode($row); 
-    // }
+    while($row=$sql->fetch(PDO::FETCH_ASSOC)){ 
 
-    $test = "[{'avalible':true,'name':'{\'Barranco\'}','picture':'{\'https://viajes.nationalgeographic.com.es/medio/2019/11/06/barranco1_efb2c25b_800x800.jpg\'}','price':'{\'s/20.00\'}'},{'avalible':true,'name':'{\'Lima\'}','picture':'{https://elviajerolibre.com/wp-content/uploads/2020/04/Lima-que-ver.jpg}','price':'{\'s/25.00\'}'},{'avalible':true,'name':'{\'Chorrillos}','picture':'{https://st.depositphotos.com/1010142/3084/i/450/depositphotos_30847313-stock-photo-coast-of-lima-peru.jpg}','price':'{\'15.00\'}'},{'avalible':true,'name':'{\'Parque de las leyendas}','picture':'{https://leyendas.gob.pe/wp-content/uploads/2022/01/PARQUE-DE-LAS-LEYENDAS-002-scaled.jpg}','price':'{\'s/50.00\'}'}]";
-    echo ($test); 
-//    header("HTTP/1.1 200 OK");
- 
+          //  $temp = mb_convert_encoding($row, 'UTF8', 'Windows-1252');
+        array_push($userData,$row);
+    }
+  
+
+    header("HTTP/1.1 200 OK");
+    echo json_encode($userData,JSON_FORCE_OBJECT); 
   exit();
 
 }
