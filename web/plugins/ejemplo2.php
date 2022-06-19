4fdd0b276ffe620fe1330122992967eac7f6ad3c
egarciae2021 <73246932@certus.edu.pe>
2022-06-19 14:55:43 -0500

    descargar pdf

--- web/plugins/ejemplo2.php
+++ web/plugins/ejemplo2.php
@@ -36,8 +36,8 @@
     $pdf->Image("/opt/lampp/htdocs/git_clone/test-educalma/web/plugins/certificate/".$nomAlumno.$codCurso.".jpg",0,0,11.7,8.27);
     $pdf->Output("/opt/lampp/htdocs/git_clone/test-educalma/web/plugins/certificate/".$nomAlumno.$codCurso.".pdf","F");
     $pdf->Output("/opt/lampp/htdocs/git_clone/test-educalma/web/plugins/certificate/".$nomAlumno.$codCurso.".pdf","D");
-    $pdf->Output(); 
+    // $pdf->Output(); 
 
-    // echo "/opt/lampp/htdocs/git_clone/test-educalma/web/plugins/certificate/".$nomAlumno.$codCurso."pdf";
+    echo "/opt/lampp/htdocs/git_clone/test-educalma/web/plugins/certificate/".$nomAlumno.$codCurso."pdf";
     imagedestroy($image);
  ?>
