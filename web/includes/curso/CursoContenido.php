5f68d6402453dc166feb4b9d920e541acd8e3856
egarciae2021 <73246932@certus.edu.pe>
2022-06-19 13:34:45 -0500

    descargar pdf

--- web/includes/curso/CursoContenido.php
+++ web/includes/curso/CursoContenido.php
@@ -957,7 +957,7 @@
                formData.append('nombre_curso', :<?php echo $dato4['nombreCurso'] ;?>);
               formData.append('cod_alumno', <?php echo  $dato20['codigo_alumno']; ?>);
               formData.append('username', <?php echo $dato['cod_curso'] ;?>);
-              formData.append('nombre_estudiante', <?php echo $dato20['nombres'] :?>); 
+              formData.append('nombre_estudiante', <?php echo $dato20['nombres'] ;?>); 
 
               console.log(formData);
 
