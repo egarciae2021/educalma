7638e5e2b523255e392f9f7e68428a3e98318f75
JesusCRojas <cacharojasjesuseusebio@gmail.com>
2022-06-22 10:14:31 -0500

    Mejorar la posicion de los filtros de los reportes, Jesus Cacha

--- web/includes/dashboard/reporteUsuario3.php
+++ web/includes/dashboard/reporteUsuario3.php
@@ -56,59 +56,30 @@ if (isset($_SESSION['Logueado']) && ($_SESSION['Logueado'] === true)) {
                 padding: 0;
             }
 
+            .filters {
+              
+              background: #7C83FD;
+              /*border-radius: 20px !important;*/
+
+            }
+
             @media screen and (max-width: 720px) {
 
-                .dataTables_filter{
-
-                    float: right;
-                    position: relative;
-                    left: 25px;
-                }
+                 
             }
 
             @media screen and (max-width: 640px) {
 
-                .dataTables_filter{
-
-                    float: right;
-                    position: relative;
-                    left: 25px;
-                }
-            }
-
-            .filtro_1 {
-
-              display: flex;
-              color: #737BF1;
-              position: relative;
-              left: 10px;
- 
                
             }
 
-            .txtFiltrarPor {
+            @media (max-width: 429px) {
 
-              font-weight: bold;
+             
             }
 
-            .cbofiltroApDes {
-
-              position: relative;
-              top: -10px;
-              width: 270px;
-            }
-
-          @media (max-width: 429px) {
-
-            .filtro_1{
-              display: block;
-            }
-            }
-
-          @media (max-width: 429px) {
-            .cbofiltroApDes{
-              right: 11px;
-            }
+            @media (max-width: 429px) {
+             
             }
             
 
@@ -120,125 +91,109 @@ if (isset($_SESSION['Logueado']) && ($_SESSION['Logueado'] === true)) {
     <div class="container-fluid">
       <div class="row mt-5">
         <div class="col-12">
-        <div class="title" style="color:#737BF1;">Reporte de Usuarios</div>
-          <div class="row">
-            <div class="col-12">
-              <nav class="navbar navbar-expand">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link active" href="reporteUsuario.php">
-                      por usuarios
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="reporteEmpresa.php">
-                      por empresas
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
+        <div class="title" style="color:#737BF1; margin-left: 38px;">Reporte de Usuarios</div>
+          
+        
+        
+        
+        
+                  <div style="margin-left: 25px;" class="row">
+                    <div class="col-12">
+                      <nav class="navbar navbar-expand">
+                        <ul class="navbar-nav">
+                          <li class="nav-item">
+                            <a class="nav-link active" href="reporteUsuario.php">
+                              por usuarios
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="reporteEmpresa.php">
+                              por empresas
+                            </a>
+                          </li>
+                        </ul>
+                      </nav>
+                    </div>
+                  </div>
 
           
 
-                  <br><br>
+              
 
                   
 
 
 
 
-
-
-
-
-<div style="position: relative; top: -80px;" class="container mt-5">
-    <div class="col-12">
  
 
 
-    <div class="row">
-<div class="col-12 grid-margin">
-<div class="card">
-<div class="card-body">
+                  <form style="margin-left: 30px; margin-top: 30px;">
+
+                          <div class="col-12 row">
+                              <div class="col-11">
+                                          <table class="table">
+                                                  <thead>
+                                                          <tr class="filters">
+
+                                                                  <th>
+                                                                          Nombre:
+                                                                          <input type="text" id="" name="" class="form-control mt-2" value="" style="border: #bababa 1px solid; color:#000000;" >
+                                                                  </th>
+                                                                  <th>
+                                                                          Curso:
+                                                                          <input type="text" id="" name="" class="form-control mt-2" value="" style="border: #bababa 1px solid; color:#000000;" >
+                                                                  </th>
+                                                                  <th>
+                                                                          Fecha de Inscripción:
+                                                                          <input type="date" id="" name="" class="form-control mt-2" value="" style="border: #bababa 1px solid; color:#000000;" >
+                                                                  </th>
+                                                                  <th>
+                                                                          Fecha de Finalización:
+                                                                          <input type="date" id="" name="" class="form-control mt-2" value="" style="border: #bababa 1px solid; color:#000000;" >
+                                                                  </th>
+                                                                  
+                                                                  
+                                                                  <th>
+                                                                          Avance
+                                                                          <select id="" id="" name="" class="form-control mt-2" style="border: #bababa 1px solid; color:#000000;" >
+                                                                                  
+                                                                                  <option value="">Todos</option>
+                                                                                  <option value="En_Curso">En Curso</option>
+                                                                                  <option value="Terminado">Terminado (100%)</option>
+                                                                          </select>
+                                                                  </th>
+
+                                                                  <th>
+                                                                          Estado
+                                                                          <select id="" id="" name="" class="form-control mt-2" style="border: #bababa 1px solid; color:#000000;" >
+                                                                                  
+                                                                                  <option value="Todos">Todos</option>
+                                                                                  <option value="Aprobado">Aprobado</option>
+                                                                                  <option value="Desaprobado">Desaprobado</option>
+                                                                                  
+                                                                          </select>
+                                                                  </th>
+                                                          </tr>
+                                                  </thead>
+                                          </table>
+                              </div>
 
 
-<form>
-        <div class="col-12 row">
+                              <div style="margin-top: 28px;" class="col-1">
+                                  <input type="submit" class="btn" value="Ver" style="margin-top: 38px; background-color: #7C83FD; color: white;">
+                              </div>
+                              
+                          </div>
 
-
-            
-            <div class="col-11">
-
-                        <table class="table">
-                                <thead>
-                                        <tr class="filters">
-
-                                                <th>
-                                                        Nombre:
-                                                        <input type="text" id="" name="" class="form-control mt-2" value="" style="border: #bababa 1px solid; color:#000000;" >
-                                                </th>
-                                                <th>
-                                                        Curso:
-                                                        <input type="text" id="" name="" class="form-control mt-2" value="" style="border: #bababa 1px solid; color:#000000;" >
-                                                </th>
-                                                <th>
-                                                        Fecha de Inscripción:
-                                                        <input type="date" id="" name="" class="form-control mt-2" value="" style="border: #bababa 1px solid; color:#000000;" >
-                                                </th>
-                                                <th>
-                                                        Fecha de Finalización:
-                                                        <input type="date" id="" name="" class="form-control mt-2" value="" style="border: #bababa 1px solid; color:#000000;" >
-                                                </th>
-                                                
-                                                
-                                                <th>
-                                                        Avance
-                                                        <select id="" id="" name="" class="form-control mt-2" style="border: #bababa 1px solid; color:#000000;" >
-                                                                
-                                                                <option value="">Todos</option>
-                                                                <option value="En_Curso">En Curso</option>
-                                                                <option value="Terminado">Terminado (100%)</option>
-                                                        </select>
-                                                </th>
-
-                                                <th>
-                                                        Estado
-                                                        <select id="" id="" name="" class="form-control mt-2" style="border: #bababa 1px solid; color:#000000;" >
-                                                                
-                                                                <option value="Todos">Todos</option>
-                                                                <option value="Aprobado">Aprobado</option>
-                                                                <option value="Desaprobado">Desaprobado</option>
-                                                                
-                                                        </select>
-                                                </th>
-                                                
-                                               
-                                        </tr>
-                                </thead>
-                        </table>
-                </div>
-
-
-                <div style="margin-top: 28px; class="col-1">
-                    <input type="submit" class="btn" value="Ver" style="margin-top: 38px; background-color: #7C83FD; color: white;">
-                </div>
-
-
-                
-            
-            
-
-
-        
-        <p style="margin-left: 20px; font-weight: bold; color:#8CC9DB;"><i class="mdi mdi-file-document"></i> <?php ?> Resultados encontrados</p>
-</form>
+                          
+                          <p style="margin-left: 7px; font-weight: bold; color:#8CC9DB;"><i class="mdi mdi-file-document"></i> <?php ?> # Resultados encontrados</p>
+                  </form>
 
 
 
 
-                  <div class="card mt-2">
+                  <div style="margin-left: 30px;" class="card mt-2">
                     <div class="card-header">
                         <div class="row mb-2">
                             <div class="col-12">
