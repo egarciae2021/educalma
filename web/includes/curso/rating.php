0be25c51070b1230725ee82c2e762942c6b5a992
BladimirCC <bladimir_18_10@outlook.com>
2022-07-07 17:37:36 -0500

    Prueba Ranking, Bladimir Cubas Castillo

--- web/includes/curso/rating.php
+++ web/includes/curso/rating.php
@@ -1,12 +1,12 @@
 <?php 
-    if(isset($_GET['rating'])&& $_GET['coment']){
+    if(isset($_POST['rating'])&& $_POST['coment']){
         echo "guardado";
         $pdo2 = Database::connect();
         try{
             $id =$_SESSION['codUsuario'];
-            $idCurso = $_GET['id'];
-            $estrellas = $_GET['rating'];
-            $comentario_var = $_GET['coment'];
+            $idCurso = $_POST['id'];
+            $estrellas = $_POST['rating'];
+            $comentario_var = $_POST['coment'];
             $sql ="INSERT INTO puntaje_curso (id_user, id_curso, numero_estrellas, comentario) VALUES (?,?,?,?)";
             $pdo->prepare($sql)->execute([$id, $idCurso, $estrellas,$comentario_var]);
         }catch(PDOException $e){
