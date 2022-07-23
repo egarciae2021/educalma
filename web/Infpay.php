4dbd03d6764da6bf63451a19d7267257937a9fb3
Edgar Jean Pierre Garcia Egusquiza <egarcia@tci.net.pe>
2022-07-23 14:18:45 -0500

    prueba pasarela

--- web/Infpay.php
+++ web/Infpay.php
@@ -1,16 +1,34 @@
 <?php
 
-echo "Esta pagina muestras los datos de pago"."<br>";
-
+$servername = "20.226.29.168";
+$username = "root";
+$password = 'T3$t1ng.C4lm4';
+$dbname = "educalma";
+$topic ;
+$id;
+ 
+$conn = mysqli_connect($servername, $username, $password, $dbname); 
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+ 
 if (isset($_GET["topic"])) {
+    $topic = $_GET["topic"];
 echo $_GET["topic"];
 }
 
 if (isset($_GET["id"])) {
+    $id = $_GET["id"];
     echo $_GET["id"];
     }
 
-echo "<br>";
-echo "Se acabo de presentar los datos";
+
+$sql = " insert into pagos(topic,id) values ('" +   $topic       +"','"+    $id       +"')";
+
+echo $sql;
+mysqli_query($conn, $sql);
+ 
+mysqli_close($conn);   
+
 
 ?>
