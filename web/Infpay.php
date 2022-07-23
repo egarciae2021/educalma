fbe936ee1371c87429d75758dba362028b6880e1
Edgar Jean Pierre Garcia Egusquiza <egarcia@tci.net.pe>
2022-07-23 15:01:50 -0500

    prueba pasarela

--- web/Infpay.php
+++ web/Infpay.php
@@ -4,38 +4,37 @@ $servername = "20.226.29.168";
 $username = "root";
 $password = 'T3$t1ng.C4lm4';
 $dbname = "educalma";
-$topic ="" ;
+$topic = "" ;
 $id = "" ;
  
 $conn = mysqli_connect($servername, $username, $password, $dbname); 
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
  
-if (isset($_GET["topic"])) {
+if (isset($_GET["topic"]) && !empty($_GET["topic"])) {
     $topic = $_GET["topic"]; 
+
+    if (isset($_GET["id"]) && !empty($_GET["id"])) {
+        $id = $_GET["id"]; 
+        }
+    
+    
+    $sql = "INSERT INTO pagos(topic,id) VALUES ('$topic' ,'$id')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Table MyGuests created successfully";
+      } else {
+        echo "Error creating table: " . mysqli_error($conn);
+      }
+    mysqli_close($conn);   
+     
 }
 
-if (isset($_GET["id"])) {
-    $id = $_GET["id"]; 
-    }
-
-
-$sql = "INSERT INTO pagos(topic,id) VALUES ('$topic' ,'$id')";
-
-echo $sql;
 
 
 
-if (mysqli_query($conn, $sql)) {
-    echo "Table MyGuests created successfully";
-  } else {
-    echo "Error creating table: " . mysqli_error($conn);
-  }
 
-  
- 
-mysqli_close($conn);   
 
 
 ?>
